eb574f50f7dabac472437bbed0685f08fc735c73
namipapa <whybevv@gmail.com>
2024-05-14 22:17:41 -0700

    Add setup doc

---
+++ runme.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import uvicorn
+from apps.base.hello import hello, logger
+from apps.base.conf import conf, LogLevel
+from apps.route import create_app
+
+app = create_app()
+if __name__ == "__main__":
+    try:
+        hello()
+        uvicorn.run(app='runme:app',
+                    host=conf.host,
+                    port=conf.port,
+                    reload=True,
+                    log_level=LogLevel.lower(),
+                    log_config="apps/base/log_conf.json"
+                    )
+    except Exception as e:
+        logger.exception("服务异常！", e)
