66f65e1033e0b09175ec458585ce5790e08a65bf
limengunique <limengunique0917@gmail.com>
2024-05-19 21:24:41 -0700

    add basic utils to support map,login,email,database check

---
+++ common/utils/__init__.py
@@ -0,0 +1,6 @@
+from common.utils.dingding_utils import DingDingUtils
+
+from common.utils.file_utils import *
+from common.utils.time_utils import *
+from common.utils.json_utils import *
+from common.utils.common_utils import *
---
+++ common/utils/common_utils.py
@@ -0,0 +1,2 @@
+def println(message):
+  print('\033[1;32m %s \033' % message)
