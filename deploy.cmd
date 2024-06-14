303067dcc4c3249ca5b2e7abe76ebcf921329e21
Byte Wizard <46955122+bytewiz3@users.noreply.github.com>
2024-06-13 20:31:43 -0700

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,16 +1,16 @@
-gp 'test'
-git push heroku
-
-
-heroku logs --tail
-
-heroku
-
-/app/.heroku/python/lib/python3.8/site-packages/pyhanlp/static/
-下载 https://file.hankcs.com/hanlp/hanlp-1.8.4-release.zip 到 /app/.heroku/python/lib/python3.8/site-packages/pyhanlp/static/hanlp-1.8.4-release.zip
-/app/.heroku/python/lib/python3.8/site-packages/pyhanlp/static/hanlp-1.8.4-release.zip
-
-wget -o data-for-1.8.4.zip https://file.hankcs.com/hanlp/data-for-1.7.5.zip
-
-wget -o data-for-1.8.4.zip http://software.haokeniu.com/data-for-1.7.5.zip
-data-for-1.8.4.zip
+gp 'test' 
+git push heroku 
+ 
+ 
+heroku logs --tail 
+ 
+heroku 
+ 
+/app/.heroku/python/lib/python3.8/site-packages/pyhanlp/static/ 
+下载 https://file.hankcs.com/hanlp/hanlp-1.8.4-release.zip 到 /app/.heroku/python/lib/python3.8/site-packages/pyhanlp/static/hanlp-1.8.4-release.zip 
+/app/.heroku/python/lib/python3.8/site-packages/pyhanlp/static/hanlp-1.8.4-release.zip 
+ 
+wget -o data-for-1.8.4.zip https://file.hankcs.com/hanlp/data-for-1.7.5.zip 
+ 
+wget -o data-for-1.8.4.zip http://software.haokeniu.com/data-for-1.7.5.zip 
+data-for-1.8.4.zip 
