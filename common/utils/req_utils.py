51efe9f4b3ac290478099f72990a2f921ec7885a
bytewiz3 <46955122+bytewiz3@users.noreply.github.com>
2024-06-22 23:53:13 -0700

    Revert "update req_utils.py"
    This reverts commit 0381743b193d40f7ad167dca784110a8e917f12d.

--- common/utils/req_utils.py
+++ common/utils/req_utils.py
@@ -1,29 +1,29 @@
-import json 
- 
-import requests 
- 
-proxies = {"http": None, "https": None} 
- 
-_header = dict() 
-_header["Content-Type"] = "application/json" 
- 
- 
-def post_json(url, post_data, header=None): 
-    if header is None: 
-        header = dict() 
-    header = dict(_header, **header) 
- 
-    response = requests.post(url, headers=header, data=json.dumps(post_data), timeout=30, proxies=proxies) 
-    if response.status_code != 200: 
-        return None 
-    return response.text 
- 
- 
-def get(url, para): 
-    header = dict() 
-    header["Content-Type"] = "application/json" 
- 
-    response = requests.get(url, para, headers=header, proxies=proxies, timeout=30) 
-    if response.status_code != 200: 
-        return None 
-    return response.json() 
+import json
+
+import requests
+
+proxies = {"http": None, "https": None}
+
+_header = dict()
+_header["Content-Type"] = "application/json"
+
+
+def post_json(url, post_data, header=None):
+    if header is None:
+        header = dict()
+    header = dict(_header, **header)
+
+    response = requests.post(url, headers=header, data=json.dumps(post_data), timeout=30, proxies=proxies)
+    if response.status_code != 200:
+        return None
+    return response.text
+
+
+def get(url, para):
+    header = dict()
+    header["Content-Type"] = "application/json"
+
+    response = requests.get(url, para, headers=header, proxies=proxies, timeout=30)
+    if response.status_code != 200:
+        return None
+    return response.json()
